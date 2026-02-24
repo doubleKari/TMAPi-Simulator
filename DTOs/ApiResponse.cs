4885f262855994fc77b4ef0afc0b01e64594c978
Jacob Akotuah <jacobakotuah@gmail.com>
2026-02-24 16:01:46 +0000

    [TM-005-007] Create DTOs

---
+++ DTOs/ApiResponse.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TMAPi_Simulator.DTOs
+{
+    internal class ApiResponse<T>(bool success, string message, T data, int statuscode)
+    {
+        public bool Success { get; set; } = success;
+        public string Message { get; set; } = message;
+
+        public T Data { get; set; } = data;
+
+        public int StatusCode { get; set; } = statuscode;
+
+        public static ApiResponse<T> SuccessResponse(T data, string message)
+        {
+
+            return new ApiResponse<T>(true, message, data, 200);
+        }
+
+        public static ApiResponse<T> ErrorResponse (string message, T data, int statusCode)
+        {
+            return new ApiResponse<T>(false, message, data, statusCode);
+        }
+    }
+}
---
+++ DTOs/TaskResponse.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TMAPi_Simulator.DTOs
+{
+    public record TaskResponse(
+        int Id, 
+        string Title, 
+        string Description, 
+        string Status, 
+        string Priority, 
+        int AssignedToUserId, 
+        int ProjectId, 
+        DateTime CreatedDate, 
+        DateTime DueDate
+    );
+}
