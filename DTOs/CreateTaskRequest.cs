762d0884f6e3075bbf4d445a348004ae0e68639c
y2k <kaykayyaw@gmail.com>
2026-02-26 13:15:44 +0000

    [TM-007] Create Request DTOs

---
+++ DTOs/CreateTaskRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TMAPi_Simulator.DTOs
+{
+    public record CreateTaskRequest (
+        string Title, 
+        string Description, 
+        string Priority, 
+        int AssignedToUserId, 
+        int ProjectId, 
+        DateTime DueDate 
+    );
+    
+}
---
+++ DTOs/UpdateTaskRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TMAPi_Simulator.DTOs
+{
+    public record UpdateTaskRequest(
+        string Title, 
+        string Description, 
+        string Status, 
+        string Priority, 
+        int AssignedToUserId, 
+        DateTime DueDate
+    );
+}
