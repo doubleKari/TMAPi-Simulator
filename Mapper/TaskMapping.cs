7aa569fe56949fe16fd188ee11bb5c2fbaf1cc30
y2k <kaykayyaw@gmail.com>
2026-02-26 13:01:27 +0000

    [TM-008] Create TaskService - Basic Structure

---
+++ Mapper/TaskMapping.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TMAPi_Simulator.DTOs;
+using TMAPi_Simulator.Models;
+
+namespace TMAPi_Simulator.Mapper
+{
+    public static class TaskMapping
+    {
+        public static TaskResponse ToResponse(this TaskItem taskItem)
+        {
+            return new TaskResponse(
+                taskItem.Id,
+                taskItem.Title,
+                taskItem.Description,
+                taskItem.Status.ToString(),
+                taskItem.Priority.ToString(),
+                taskItem.AssignedToUserId,
+                taskItem.ProjectId,
+                taskItem.CreatedDate,
+                taskItem.DueDate
+             );
+        
+        }
+    }
+}
---
+++ Services/TaskService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TMAPi_Simulator.DTOs;
+using TMAPi_Simulator.Models;
+
+namespace TMAPi_Simulator.Services
+{
+    public class TaskService
+    {
+        private readonly List<TaskItem> _tasks;
+        private readonly int _nextId = 1;
+
+        public TaskService()
+        {
+            _tasks = [];
+            _nextId = 1;
+        }
+
+        public ApiResponse<TaskResponse> CreateTask(CreateTaskRequest request)
+        {
+            if (!string.IsNullOrWhiteSpace(request.Title))
+            {
+                //parse priority string to priority enum
+
+            }
+        }
+
+        
+
+    }
+}
