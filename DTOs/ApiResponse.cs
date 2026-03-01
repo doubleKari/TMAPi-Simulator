017b818deea2f20afed903894faf4fc1ad212ae0
y2k <kaykayyaw@gmail.com>
2026-03-01 01:02:07 +0000

    [TM-005] Revise ApiResonse Generic class

--- DTOs/ApiResponse.cs
+++ DTOs/ApiResponse.cs
@@ -4,24 +4,20 @@ using System.Text;
 
 namespace TMAPi_Simulator.DTOs
 {
-    internal class ApiResponse<T>(bool success, string message, T data, int statuscode)
+    public class ApiResponse<T>(bool success, string message, T? data, int statuscode)
     {
         public bool Success { get; set; } = success;
         public string Message { get; set; } = message;
-
-        public T Data { get; set; } = data;
-
+        public T? Data { get; set; } = data;
         public int StatusCode { get; set; } = statuscode;
-
         public static ApiResponse<T> SuccessResponse(T data, string message)
         {
 
             return new ApiResponse<T>(true, message, data, 200);
         }
-
-        public static ApiResponse<T> ErrorResponse (string message, T data, int statusCode)
+        public static ApiResponse<T> ErrorResponse (string message, int statusCode)
         {
-            return new ApiResponse<T>(false, message, data, statusCode);
+            return new ApiResponse<T>(false, message, default, statusCode);
         }
     }
 }
